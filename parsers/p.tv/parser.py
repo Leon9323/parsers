c9db7ff2709cd837ed4f953e6ef35a3f03ebb876
Leon9323 <spider9323@gmail.com>
2015-09-15 19:18:48 +0600

    parse of site
    parse of site

---
+++ parsers/p.tv/parser.py
@@ -0,0 +1,72 @@
+import urllib
+import json
+import datetime
+from lxml.html import fromstring
+from lxml.html import tostring
+def getDate(day, month, year):
+    months=['Января','Февраля','Марта','Апреля','Майя','Июня','Июля','Августа','Сентября','Октября','Ноября','Декабря'];
+    month = int(month) - 1;
+    return str(day+" "+months[month]+" "+year);
+def getData(url):
+    host = "pornoboss.tv"
+    html = urllib.urlopen(url).read();
+    page = fromstring(html);
+    UrlName = ".//*[@id='content']/div[2]/div[2]/div[1]/h1/text()";
+    header = page.xpath(UrlName);
+    #print unicode(header[0]);
+    
+    UrlCategories = ".//*[@id='content']/div[2]/div[2]/div[3]/a/text()";
+    categories = page.xpath(UrlCategories);
+    UrlLinkCategories = ".//*[@id='content']/div[2]/div[2]/div[3]/a/@href";
+    link = page.xpath(UrlLinkCategories);
+    catArr = [];
+    iteration = 0;
+    for item in link:
+        catArr.append({"name": categories[iteration].encode("cp1251"), "link": link[iteration].encode("cp1251")});
+        iteration=iteration+1;
+        
+    UrlDate = ".//*[@id='content']/div[2]/div[2]/div[3]/text()";
+    date = page.xpath(UrlDate);
+    mydate = date[0].encode("cp1251").split(",");
+    if(mydate[0]=="Сегодня"):
+        dateArr = str(datetime.date.today()).split("-");
+        dateVideo = getDate(dateArr[2], dateArr[1], dateArr[0]);
+    else:
+        if(mydate[0]=="Вчера"):
+            dateArr = str(datetime.date.today() - datetime.timedelta(days=1)).split("-");
+            dateVideo = getDate(dateArr[2], dateArr[1], dateArr[0]);
+        else:
+            dateArr = str(mydate[0]).split("-");
+            dateVideo = getDate(dateArr[0], dateArr[1], dateArr[2]);
+    #print date[0].encode("cp1251");
+            
+    UrlDescription = ".//*[@class='img']/div[1]/div[1]/text()";
+    description = page.xpath(UrlDescription);
+    #print description[0].encode("cp1251");
+    result = {"host": host, "url":url, "header": header[0].encode("cp1251"), "categories": catArr, "date": str(dateVideo),
+              "description": description[0].encode("cp1251")};
+    return result;
+def getResult(url):
+    html = urllib.urlopen(url).read();
+    page = fromstring(html)
+    page.make_links_absolute(url)
+    PathOnPage = ".//*[@id='content']/div/div/div/a/@href";
+    UrlVideo = page.xpath(PathOnPage);
+    #jsonResult = [];
+    for item in UrlVideo:
+        #jsonResult.append(getData(item));
+        with open('data.json', 'a') as outfile:json.dump(getData(item), outfile, sort_keys = True, indent = 4, ensure_ascii = False);
+        f = open('data.json', 'a');
+        f.write(",");
+        f.close();
+def main():
+    i=1;
+    url = "http://pornoboss.tv";
+    html = urllib.urlopen(url).read();
+    page = fromstring(html);
+    path = ".//*[@id='content']/div[2]/div[21]/a[10]/text()";
+    last = page.xpath(path);
+    while i<=int(last[0]):
+        getResult("http://pornoboss.tv/page/"+str(i));
+        i=i+1;
+main();
